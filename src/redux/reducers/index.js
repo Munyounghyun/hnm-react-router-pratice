afb7287d90d96a557e2bb3ce3b9be8a846201421
Munyounghyun <fldh3369@naver.com>
2022-08-12 16:52:28 +0900

    reducers/index.js 삭제

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authenticateReducer from "./authenticateReducer";
-import productReducer from "./productReducer";
-
-export default combineReducers({
-  //키 : 원하는 Reducer
-  auth: authenticateReducer,
-  product: productReducer,
-});
